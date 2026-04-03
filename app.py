eb072467048cdbfba1bbee22af6b1d995bbfe942
SGHPK0905 <sonpropk@gmail.com>
2026-04-03 23:46:33 +0700

    edit link music

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import random
 import json
 import os
+import re
 import streamlit.components.v1 as components
 
 MY_DISCORD_ID = "<@472746897812226059>" 
@@ -106,11 +107,18 @@ st.divider()
 
 # --- TRẠM PHÁT NHẠC ---
 st.header("🎵 Playlist chung của mình nè!")
-spotify_link = user_data.get("spotify_url", "")
-if spotify_link:
+raw_spotify_link = user_data.get("spotify_url", "")
+
+if raw_spotify_link:
+    embed_link = raw_spotify_link.strip().split("?")[0]
+    
+    embed_link = re.sub(r'intl-[a-zA-Z0-9]+/', '', embed_link)
+    
+    if "spotify.com/" in embed_link and "/embed/" not in embed_link:
+        embed_link = embed_link.replace("spotify.com/", "spotify.com/embed/")
         
     components.html(
-        f'<iframe style="border-radius:12px" src="{spotify_link}" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+        f'<iframe style="border-radius:12px" src="{embed_link}" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
         height=360
     )
 st.divider()
